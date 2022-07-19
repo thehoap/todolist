6150b217c46688445ece37c2faa4968d5f374565
thehoap <thehoap.2000@gmail.com>
2022-07-19 09:36:19 +0700

    init and import Routers into app

--- src/layouts/MainLayout/MainLayout.jsx
+++ src/layouts/MainLayout/MainLayout.jsx
@@ -1,30 +1,19 @@
 import React from "react";
 import Header from "../../components/Header";
-import { BasedInput } from "../../components/Input/styles";
-import Radio from "../../components/Radio";
-import RadioGroup from "../../components/RadioGroup";
 import Sidebar from "../../components/Sidebar";
 import "./MainLayout.scss";
 
-const MainLayout = ({ header, sidebar, main }) => {
-    const headerElement = header ?? <Header />;
-    const sidebarElement = sidebar ?? <Sidebar />;
-
+const MainLayout = ({ children }) => {
     return (
         <div className="main-layout">
-            <div className="main-layout__header">{headerElement}</div>
+            <div className="main-layout__header">
+                <Header />
+            </div>
             <div className="main-layout__container">
-                <div className="main-layout__sidebar">{sidebarElement}</div>
-                {/* <div className="main-layout__main">{main}</div> */}
-                <div className="main-layout__main">
-                    <BasedInput
-                        type="text"
-                        placeholder="Enter your title"
-                        name="title"
-                        id="title"
-                    />
-                    <RadioGroup/>
+                <div className="main-layout__sidebar">
+                    <Sidebar />
                 </div>
+                <div className="main-layout__main">{children}</div>
             </div>
         </div>
     );
---
+++ src/routes/MainRoute.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MainRoute = ({ layout: Layout, component: Component }) => {
+    return <Layout><Component /></Layout>;
+};
+
+export default MainRoute;
