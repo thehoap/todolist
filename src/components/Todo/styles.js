5f9f67a0f5ba04a3a5220d884e32345980e51886
thehoap <thehoap.2000@gmail.com>
2022-07-20 11:08:14 +0700

    styling grid  list of todos

--- src/components/Todo/styles.js
+++ src/components/Todo/styles.js
@@ -1,8 +1,32 @@
 import * as COLOR from "constants/color";
 import styled from "styled-components";
 
+const border = `2px solid ${COLOR.PRIMARY}`;
+
 const STodo = styled.div`
     padding: 12px;
+    display: inline-block;
+    height: 160px;
+    border-radius: 10px;
+    border: ${border};
+
+    & > hr {
+        height: 2px;
+        background-color: ${COLOR.PRIMARY};
+        border: 0;
+        outline: none;
+    }
 `;
 
 export default STodo;
+
+export const STodoText = styled.p`
+    font-weight: ${(props) => props.fontWeight || 400};
+    line-height: 1.4;
+    color: ${(props) => props.color || "#000"};
+    margin-bottom: 0;
+    display: -webkit-box;
+    -webkit-line-clamp: ${props => props.lineClamp};
+    -webkit-box-orient: vertical;
+    overflow: hidden;
+`;
--- src/constants/color.js
+++ src/constants/color.js
@@ -2,3 +2,9 @@ export const PRIMARY = "#4F98CA";
 export const SECONDARY = "#EFFFFB";
 export const TEXT = "#272727";
 export const SIDEBAR = "#50D890";
+
+export const COLOR_STATUS = {
+    newtask: "#FF0000",
+    doingtask: "#FAC213",
+    donetask: "#49FF00",
+};
